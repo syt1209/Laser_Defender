8f62513425e8eb3553670be4b33cb092fc199d76
syt1209 <syt1209@gmail.com>
2020-11-11 11:26:56 -0500

    Player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 posChange = _speed * direction * Time.deltaTime;
+        transform.position += posChange;
+    }
+}
