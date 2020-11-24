de3d10ade1674de6522695547c432a8609f9804d
syt1209 <syt1209@gmail.com>
2020-11-23 22:45:34 -0500

    Enemy path added

---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _wayPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
