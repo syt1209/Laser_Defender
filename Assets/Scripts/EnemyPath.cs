2dde7ca595f7c9d302e4170eb8f3a2b200b52036
syt1209 <syt1209@gmail.com>
2020-11-26 22:44:32 -0500

    Enemy movement path added

--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class EnemyPath : MonoBehaviour
 {
     [SerializeField] private List<Transform> _wayPoints;
+    [SerializeField] private float _speed = 2f;
+    private int _waypointIndex = 0;
+
+
     void Start()
     {
-        
+        transform.position = _wayPoints[_waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_waypointIndex <= _wayPoints.Count - 1)
+        {
+            var targetPosition = _wayPoints[_waypointIndex].transform.position;
+            var movementThisFrame = _speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                _waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
